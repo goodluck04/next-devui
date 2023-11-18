cb5fbdc30fc39b1a2d5501ab4d584956e72a86b2
goodluck04 <goodluckkumar04@gmail.com>
2023-11-18 09:13:35 +0530

    add prisma build command

--- src/app/api/post/search/route.ts
+++ src/app/api/post/search/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     const url = new URL(request.url)
     const query = url.searchParams.get("query")
-    const posts = await prisma.post.findMany({
+    const posts = await NextUI({
         where: {
             title: {
                 contains: query ?? "" //if query then search or not
